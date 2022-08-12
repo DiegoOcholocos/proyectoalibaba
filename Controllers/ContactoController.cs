dfbdc95630fc82d2f523c8ea8d4be845820bf596
diegocho <diegoalonzo2001@hotmail.com>
2022-08-12 11:45:40 -0500

    Parte2

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -14,14 +14,11 @@ namespace alibabaproy2022_v2.Controllers
 {
     public class ContactoController: Controller
     {
-        private readonly ILogger<ContactoController> _logger;
         private readonly ApplicationDbContext _context;
 
-        public ContactoController(ApplicationDbContext context,
-            ILogger<ContactoController> logger)
+        public ContactoController(ApplicationDbContext context)
         {
             _context = context;
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -33,10 +30,153 @@ namespace alibabaproy2022_v2.Controllers
         [HttpPost]
         public IActionResult Create(Contacto objContacto)
         {
-                _context.Add(objContacto);
-                _context.SaveChanges();
-                ViewData["Message"] = "Se registro el contacto";
-                return View("Index");
+            _context.Add(objContacto);
+            _context.SaveChanges();
+            ViewData["Message"] = "Se registro el contacto";
+            return View("Index");
+        }
+
+
+
+        public async Task<IActionResult> Indexadmin(){
+        var items = from o in _context.DataContactos select o;
+        return View(await items.OrderByDescending(w => w.Id).ToListAsync());
+        }
+        public async Task<IActionResult> IndexadminResueltos(){
+        var items = from o in _context.DataContactos select o;
+        items = items.Where(s => s.Status.Contains("RESUELTO"));
+        return View(await items.OrderByDescending(w => w.Id).ToListAsync());
+        }
+        public async Task<IActionResult> IndexadminSinResolver(){
+        var items = from o in _context.DataContactos select o;
+        items = items.Where(s => s.Status.Contains("SIN_RESOLVER"));
+        
+        return View(await items.OrderByDescending(w => w.Id).ToListAsync());
+        }
+        public async Task<IActionResult> IndexadminPendientes(){
+        var items = from o in _context.DataContactos select o;
+        items = items.Where(s => s.Status.Contains("PENDIENTE"));
+        
+        return View(await items.OrderByDescending(w => w.Id).ToListAsync());
+        }
+
+
+
+        // GET: Produtos/Delete/5
+
+        public async Task<IActionResult> Delete(int? id)
+
+        {
+
+            if (id == null)
+
+            {
+
+                return NotFound();
+
+            }
+
+
+
+            var produto = await _context.DataContactos
+
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (produto == null)
+
+            {
+
+                return NotFound();
+
+            }
+
+
+
+            return View(produto);
+
+        }
+
+
+
+        // POST: Produtos/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeleteConfirmed(int id)
+
+        {
+
+            var produto = await _context.DataContactos.FindAsync(id);
+
+            _context.DataContactos.Remove(produto);
+
+            await _context.SaveChangesAsync();
+
+
+
+            return RedirectToAction(nameof(Indexadmin));
+
+        }
+
+
+
+
+
+
+
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = await _context.DataContactos.FindAsync(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+            return View(contacto);
+        }
+    
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,name,email,numero,subject,comment,AnotacionAdmin,Status")] Contacto contacto)
+        {
+            if (id != contacto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contacto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContactoExists(contacto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Indexadmin));
+            }
+            return View(contacto);
+        }
+
+        private bool ContactoExists(int id)
+        {
+            return _context.DataContactos.Any(e => e.Id == id);
         }
     }
 }
